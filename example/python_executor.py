d703f176613150ee48c162a48d7fd1d54c921fef
PiggyPig <98620482+PiggyPigCute@users.noreply.github.com>
2022-07-12 18:57:11 +0200

    Create the python executor of the example
    This file presents the two methods to run LineCode

---
+++ example/python_executor.py
@@ -0,0 +1,22 @@
+
+
+# --- METHOD 1 ---
+
+
+# import the linecodexe() function from the linecode.py file
+from linecode import linecodexe
+
+# add the path of your LineCode file
+linecodexe(r"./my_code.lcd")
+
+
+
+
+# --- METHOD 2 ---
+
+
+# import the linecode() function from the linecode.py file
+from linecode import linecode
+
+# add your LineCode
+linecode("vanvbnvcnvdnrarbrcsd-^vbn2*n4*vavcw6cDssc=ssvdslc!<vdn0w14cxc1ssc=ss/-*z1vb\\vd*n2vaslcxc2ssc=ss/+*z1vb\\vd*n2vaslc<vdn0w16cacucccucncesscscoclcuctcicocnsl")
