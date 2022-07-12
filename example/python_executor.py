c0b12f0c070a861f14c06e1aead7d07bf2fbb1ed
PiggyPig <98620482+PiggyPigCute@users.noreply.github.com>
2022-07-12 19:49:47 +0200

    example code update
     - add the "d==0" case
     - from "aucune solution" to "no solution"

--- example/python_executor.py
+++ example/python_executor.py
@@ -7,7 +7,7 @@
 from linecode import linecodexe
 
 # add the path of your LineCode file
-linecodexe(r"./my_code.linecode")
+linecodexe(r"./my_code.lcd")
 
 
 
@@ -19,4 +19,5 @@ linecodexe(r"./my_code.linecode")
 from linecode import linecode
 
 # add your LineCode
-linecode("vanvbnvcnvdnrarbrcsd-^vbn2*n4*vavcw6cDssc=ssvdslc!<vdn0w14cxc1ssc=ss/-*z1vb\\vd*n2vaslcxc2ssc=ss/+*z1vb\\vd*n2vaslc<vdn0w16cacucccucncesscscoclcuctcicocnsl")
+linecode("vanvbnvcnvdnrarbrcsd-^vbn2*n4*vavcw6cDssc=ssvdslc>vdn0w14cxc1ssc=ss/-*z1vb\\vd*n2vaslcxc2ssc=ss/+*z1vb\\vd*n2vaslc=vdn0w6cxssc=ss/*z1vb*n2vaslc<vdn0w12cncosscscoclcuctcicocnsl")
+# /!\ WARNING: put double backslash \\ for the squareroots
